35673a4deed69318821dca3d84849022d3dceb3c
Lawrence Tsungu <tsungulawrence@gmail.com>
2023-03-17 15:57:57 -0700

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - get alphabet function
+ * Return: always 0
+ */
+int main(void)
+{
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
