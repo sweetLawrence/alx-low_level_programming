15876ad3c5ca8ee6391b0963e0d689377d2d33fa
Lawrence Tsungu <tsungulawrence@gmail.com>
2023-04-03 06:52:03 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_strchr - a function that locates a char in string
+ *@s: string for search
+ *@c: character for search
+ *
+ *Return: pointer fo 1st occ of c
+ */
+char *_strchr(char *s, char c)
+{
+    int i = 0;
+
+    while (s[i]!='\0')
+    {
+        if (s[i]==c)
+        {
+            return (&s[i]);
+        }
+        else
+        {
+            return (NULL);
+        }
+        i++;
+    }
+    return (0);
+}
