4e6e0bf7fa35a69aed6cdb7873d3a044d78248d3
Lawrence Tsungu <tsungulawrence@gmail.com>
2023-04-04 13:59:48 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+i#include "main.h"
+/**
+ * _strspn - function to check;
+ * @s: string input
+ * @accept: string to be checked;
+ * Return: counted occurences
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int cnt = 0;
+	int iter;
+
+	while (*s)
+	{
+		for (iter = 0; accept[iter]; r++)
+		{
+			if (*s == accept[iter])
+			{
+				cnt++;
+				break;
+			}
+			else if (accept[iter + 1] == '\0')
+				return (cnt);
+		}
+		s++;
+	}
+	return (cnt);
+}
