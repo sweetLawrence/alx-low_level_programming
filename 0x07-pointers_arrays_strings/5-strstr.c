9d447db286143b0cce98ae0c4bc551c7dab37537
Lawrence Tsungu <tsungulawrence@gmail.com>
2023-04-04 14:35:24 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - extract substr if success
+ * @haystack: word
+ * @needle: subst
+ * Return: aways 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *word = haystack;
+		char *substr = needle;
+
+		while (*word == *p && *substr != '\0')
+		{
+			word++;
+			substr++;
+		}
+
+		if (*substr == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
